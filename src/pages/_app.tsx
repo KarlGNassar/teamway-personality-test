61848455399093cc3142684c6e72221d06ba4da8
KarlGNassar <kgnassar01@gmail.com>
2023-03-31 10:32:28 +0300

    setup(project): setting up mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,53 @@
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      theme={{
+        colors: {
+          primary: [
+            "#e7dfc6",
+            "#e9e2cc",
+            "#ece5d1",
+            "#eee9d7",
+            "#f1ecdd",
+            "#f3efe3",
+            "#f5f2e8",
+            "#f8f5ee",
+            "#faf9f4",
+            "#fdfcf9",
+          ],
+          secondary: [
+            "#ab81cd",
+            "#b38ed2",
+            "#bc9ad7",
+            "#c4a7dc",
+            "#cdb3e1",
+            "#d5c0e6",
+            "#ddcdeb",
+            "#e6d9f0",
+            "#eee6f5",
+            "#f7f2fa",
+          ],
+          tertiary: [
+            "#0047ab",
+            "#1a59b3",
+            "#336cbc",
+            "#4d7ec4",
+            "#6691cd",
+            "#80a3d5",
+            "#99b5dd",
+            "#b3c8e6",
+            "#ccdaee",
+            "#e6edf7",
+          ],
+        },
+        primaryShade: 0,
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
